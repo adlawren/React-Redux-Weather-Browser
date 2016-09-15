9ebbda76d643420a2a4a1fe5e960c7c8812f3b77
adlawren <adlawren010@gmail.com>
2016-09-14 23:01:43 -0700

    Add form submit handler

--- src/containers/container_search_bar.js
+++ src/containers/container_search_bar.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
 
 export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+
+    // TODO: Fetch weather data
+  }
+
+  onInputChange(event) {
+    this.setState({ term: event.target.value });
+  }
+
   render() {
-    return <form className="input-group">
-      <input />
+    return <form
+      className="input-group"
+      onSubmit={this.onFormSubmit}>
+      <input
+        className="form-control"
+        value={this.state.term}
+        onChange={this.onInputChange.bind(this)}
+        placeholder="Get a five-day forecast in your favorite cities"
+        />
       <span className="input-group-btn">
         <button type="submit" className="btn btn-secondary">Submit</button>
       </span>
