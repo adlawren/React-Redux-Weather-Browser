3c66380af0bfceccf7293b190157a561455d17a4
adlawren <adlawren010@gmail.com>
2016-09-15 20:52:00 -0700

    Add working AJAX call

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import Axios from 'axios';
+
 const OPEN_WEATHER_MAP_API_KEY = '788939905b612b70dbebdda09880f000';
+const OPEN_WEATHER_MAP_ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${OPEN_WEATHER_MAP_API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${OPEN_WEATHER_MAP_ROOT_URL}&q=${city},ca`;
+  const request = Axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
--- src/containers/container_search_bar.js
+++ src/containers/container_search_bar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props) {
     super(props);
 
@@ -10,7 +13,8 @@ export default class SearchBar extends Component {
   onFormSubmit(event) {
     event.preventDefault();
 
-    // TODO: Fetch weather data
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: '' });
   }
 
   onInputChange(event) {
@@ -20,7 +24,7 @@ export default class SearchBar extends Component {
   render() {
     return <form
       className="input-group"
-      onSubmit={this.onFormSubmit}>
+      onSubmit={this.onFormSubmit.bind(this)}>
       <input
         className="form-control"
         value={this.state.term}
@@ -33,3 +37,9 @@ export default class SearchBar extends Component {
     </form>
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
