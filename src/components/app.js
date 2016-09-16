91ed14cdf9a6da3740ab652d0d12f24d5eb53c0f
adlawren <adlawren010@gmail.com>
2016-09-15 21:44:58 -0700

    Add sparklines support

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 import SearchBar from '../containers/container_search_bar';
+import WeatherList from '../containers/container_weather_list';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <SearchBar />
+        <WeatherList />
       </div>
     );
   }
